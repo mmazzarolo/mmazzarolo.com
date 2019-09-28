6d0fabdd7a0600811bfa4ec71561dde8a4eee59f
mmazzarolo <mazzarolomatteo@gmail.com>
2019-09-28 16:28:06 +0200

    Removed unused manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,17 +143,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`
-      }
-    },
     { resolve: `gatsby-plugin-styled-components`, options: { display: true } }
   ]
 };
